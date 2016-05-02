e3808c73e02e4f69116e42d3abee9bbc0de781b6
Connor Boesch <connor_boesch@yahoo.com>
2016-05-02 13:33:41 -0500

    Functionality added to RomanNumeralInterpereter

--- RomanNumeralInterpereter/Program.cs
+++ RomanNumeralInterpereter/Program.cs
@@ -1,7 +1,26 @@
-﻿namespace RomanNumeralInterpereter {
+﻿using System;
+using System.Collections.Generic;
+namespace RomanNumeralInterpereter {
     class Program {
         static void Main(string[] args) {
+            string roman = "MCMXXVIIII";
+            Context context = new Context(roman);
+            //Build the 'parse tree'
+            List<Expression> tree = new List<Expression>();
+            tree.Add(new ThousandExpression());
+            tree.Add(new HundredExpression());
+            tree.Add(new TensExpression());
+            tree.Add(new OneExpression());
 
+            //Interperet
+            foreach(Expression e in tree) {
+                e.Interperet(context);
+            }
+
+            Console.WriteLine($"{roman} = {context.Output}");
+
+            //Wait for user
+            Console.ReadKey();
         }
     }
 }
