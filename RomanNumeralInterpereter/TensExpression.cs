fd81a0e6a63c0e019f2ed5e33ac0902e25f7a988
Connor Boesch <connor_boesch@yahoo.com>
2016-05-02 13:20:58 -0500

    Functionality for 10-99 Roman Numerals.

---
+++ RomanNumeralInterpereter/TensExpression.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace RomanNumeralInterpereter {
+    class TensExpression:Expression {
+        public override String Five() {
+            return "L";
+        }
+        public override String Four() {
+            return "XL";
+        }
+        public override Int32 Multiplier() {
+            return 10;
+        }
+        public override String Nine() {
+            return "XC";
+        }
+        public override String One() {
+            return "X";
+        }
+    }
+}
